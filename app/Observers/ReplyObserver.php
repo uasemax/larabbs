93f7a223bf6448b88bbf806d8518b2ac4e0ce72e
vagrant <vagrant@homestead>
2019-04-09 14:38:07 +0000

    删除评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -12,8 +12,7 @@ class ReplyObserver
 {
     public function created(Reply $reply)
     {
-        $reply->topic->reply_count = $reply->topic->replies->count();
-        $reply->topic->save();
+        $reply->topic->updateReplyCount();
 
         $reply->topic->user->notify(new TopicReplied($reply));
     }
@@ -21,5 +20,10 @@ class ReplyObserver
 	public function creating(Reply $reply)
 	{
 		$reply->content = clean($reply->content, 'user_topic_body');
+    }
+
+	public function deleted(Reply $reply)
+	{
+		$reply->topic->updateReplyCount();
     }
 }
