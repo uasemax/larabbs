855fcc9009be5b89318234237f58b09aa8e8d5b8
vagrant <vagrant@homestead>
2019-04-14 09:05:18 +0000

    侧边栏链接

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,19 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Link;
 use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-	public function show(Category $category, Request $request, Topic $topic, User $user)
+	public function show(Category $category, Request $request, Topic $topic, User $user, Link $link)
 	{
 		$topics = $topic->withOrder($request->order)
 						->where('category_id', $category->id)
 						->paginate(20);
 		$active_users = $user->getActiveUsers();
+		$links = $link->getAllCached();
 
-		return view('topics.index', compact('topics', 'category', 'active_users'));
+		return view('topics.index', compact('topics', 'category', 'active_users', 'links'));
     }
 }
